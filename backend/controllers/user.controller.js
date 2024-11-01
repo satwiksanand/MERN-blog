a5a0843f5ac72615b5f77f2437759d8016f2cb75
satwik sanand <sanandsatwik421@gmail.com>
2024-11-01 11:26:13 +0000

    completed the user controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,6 @@
 const { user } = require("../db/index");
 const customError = require("../utils/customError");
-
+const bcryptjs = require("bcryptjs");
 const getUserById = async (req, res, next) => {
   //the functionality to look for a certain user should only be allowed for the admin.
   const userId = req.params.userId;
@@ -36,7 +36,82 @@ const getAllUsers = async (req, res, next) => {
   }
 };
 
+const signout = (req, res, next) => {
+  try {
+    res.clearCookie("access_token").json({
+      message: "signout successfull!",
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const updateUser = async (req, res, next) => {
+  const userId = req.params.userId;
+  try {
+    if (req.user.id != userId && !req.user.isAdmin) {
+      throw customError(411, "you are not allowed to update the user!");
+    }
+    if (req.body.password) {
+      if (req.body.password.length < 8) {
+        throw customError(413, "Password must be more than 8 characters!");
+      }
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
+    }
+    if (req.body.username) {
+      //find a user with the requested username
+      if (req.body.username.includes(" ")) {
+        throw customError(400, "Username cannot contain spaces");
+      }
+      if (req.body.username !== req.body.username.toLowerCase()) {
+        throw customError(400, "Username must be lowercase");
+      }
+      if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
+        throw customError(400, "Username can only contain letters and numbers");
+      }
+      const existingUser = await user.findOne({ username: req.body.username });
+      if (existingUser && existingUser._id != req.user.id) {
+        throw customError(411, "Username already in use");
+      }
+    }
+    const updatedUser = await user.findByIdAndUpdate(
+      req.user.id,
+      {
+        $set: {
+          username: req.body.username,
+          password: req.body.password,
+          profilePicture: req.body.profilePicture,
+        },
+      },
+      { new: true }
+    );
+
+    const { password: pass, ...rest } = updatedUser._doc;
+    res.json(rest);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const deleteUser = async (req, res, next) => {
+  const userId = req.params.userId;
+  try {
+    if (req.user.id != userId && !req.user.isAdmin) {
+      throw customError(411, "you are not allowed to delete the user!");
+    }
+    await user.findByIdAndDelete(userId);
+    res.json({
+      message: "User has been deleted!",
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
 module.exports = {
   getAllUsers,
   getUserById,
+  signout,
+  updateUser,
+  deleteUser,
 };
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,9 +1,18 @@
 const { Router } = require("express");
 const userRouter = Router();
 const verifyUser = require("../middleware/verifyUser.middleware");
-const { getUserById, getAllUsers } = require("../controllers/user.controller");
+const {
+  getUserById,
+  getAllUsers,
+  signout,
+  updateUser,
+  deleteUser,
+} = require("../controllers/user.controller");
 
 userRouter.get("/getUserById/:userId", verifyUser, getUserById);
 userRouter.get("/getAllUsers", verifyUser, getAllUsers);
+userRouter.post("/signout", signout);
+userRouter.put("/update/:userId", verifyUser, updateUser);
+userRouter.delete("/delete/:userId", verifyUser, deleteUser);
 
 module.exports = userRouter;
