e439da3a4c42f4982f64aa6a1a9616e88dbec187
satwik sanand <sanandsatwik421@gmail.com>
2024-11-12 11:45:08 +0000

    created basic form for categorizing blog

--- frontend/src/pages/Blogs.jsx
+++ frontend/src/pages/Blogs.jsx
@@ -1,15 +1,28 @@
 function Blogs() {
-  async function handleClick() {
-    const res = await fetch("http://localhost:3000/api/v1/user/test", {
-      credentials: "include",
-    });
-    const data = await res.json();
-    console.log(data);
-  }
-
   return (
     <div>
-      <button onClick={handleClick}>click me to fetch data!</button>
+      <form className="flex items-center justify-between bg-[#262626] p-4">
+        <p>This is the Blog form</p>
+        <div className="flex flex-col">
+          <label
+            htmlFor="category"
+            className="font-thin text-[#7e7e81] sm:text-lg sm:font-semibold"
+          >
+            Chose your Category
+          </label>
+          <select
+            name="category"
+            id="category"
+            className="rounded-lg bg-[#757575] hover:cursor-pointer"
+          >
+            <option value="All">All</option>
+            <option value="ReactJS">ReactJS</option>
+            <option value="NodeJS">NodeJS</option>
+            <option value="NextJS">NextJS</option>
+            <option value="MERN">MERN</option>
+          </select>
+        </div>
+      </form>
     </div>
   );
 }
