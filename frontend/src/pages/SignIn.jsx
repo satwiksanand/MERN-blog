6536e323fddcebd3f87b6944862364b514bfc809
satwik sanand <sanandsatwik421@gmail.com>
2024-11-14 22:41:53 +0000

    modified the ui of sign-in and sign-up page

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,19 +1,17 @@
-import {
-  Button,
-  Checkbox,
-  Label,
-  TextInput,
-  Card,
-  Spinner,
-} from "flowbite-react";
+import { Spinner } from "flowbite-react";
 import { useForm } from "react-hook-form";
 import { useDispatch, useSelector } from "react-redux";
 import { signInFailure, signInStart, signInSuccess } from "../slice/userSlice";
 import { toast } from "react-toastify";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignIn() {
-  const { register, handleSubmit, setValue } = useForm();
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm();
 
   const dispatch = useDispatch();
   const { loading } = useSelector((state) => state.user);
@@ -58,68 +56,78 @@ export default function SignIn() {
   };
 
   return (
-    <Card className="flex max-w-md items-center justify-center bg-[#1a1a1a]">
+    <div className="flex items-center justify-center p-4 sm:p-12">
       <form
-        className="flex max-w-md flex-col gap-4"
+        className="flex max-w-full flex-grow flex-col gap-4 rounded-xl border-[1px] p-6 outline-gray-500/50 sm:p-8 md:max-w-[60%] md:p-10 lg:max-w-[45%] lg:p-12"
         onSubmit={handleSubmit(onSubmit)}
       >
-        <div className="flex flex-col items-center gap-2">
-          <p className="text-2xl font-bold text-white">
-            Log in to your Account
-          </p>
-          <p className="text-lg font-bold text-[#7e7e81]">
-            Welcome Back! Please Enter Your Details
-          </p>
+        <div className="mt-2 flex flex-col items-center justify-center gap-2 text-xl font-semibold">
+          <span className="text-3xl font-bold">Log in to your Account</span>
+          Welcome Back! Please Enter Your Details
         </div>
-        <div>
-          <div className="mb-2 block">
-            <Label
-              htmlFor="useremail"
-              value="Your email"
-              className="text-white"
-            />
-          </div>
-          <TextInput
+        <div className="flex flex-col gap-2">
+          <label htmlFor="useremail" className="text-lg font-semibold">
+            Your Email
+          </label>
+          <input
             id="useremail"
+            name="useremail"
             type="email"
             placeholder="name@flowbite.com"
             required
-            {...register("useremail", { required: true })}
+            className="rounded-lg bg-[#262626] p-2 font-thin placeholder:font-thin placeholder:text-[#b3b3b3] focus:outline-none"
+            {...register("useremail", { required: "User Email is required" })}
           />
+          {errors.useremail && (
+            <p className="font-semibold text-red-600">
+              <span className="font-bold">Oops </span>
+              {errors.useremail.message}
+            </p>
+          )}
         </div>
-        <div>
-          <div className="mb-2 block">
-            <Label
-              htmlFor="password"
-              value="Your password"
-              className="text-white"
-            />
-          </div>
-          <TextInput
+
+        <div className="flex flex-col gap-2">
+          <label htmlFor="password" className="text-lg font-semibold">
+            Password
+          </label>
+          <input
             id="password"
+            name="password"
             type="password"
             required
-            {...register("password", { required: true })}
+            className="rounded-lg bg-[#262626] p-2 font-thin placeholder:font-thin placeholder:text-[#b3b3b3] focus:outline-none"
+            {...register("password", {
+              required: "Password is a necessary field!",
+            })}
           />
+          {errors.password && (
+            <p className="font-semibold text-red-600">
+              <span className="font-bold">Oops </span>
+              {errors.password.message}
+            </p>
+          )}
         </div>
-        <div className="flex items-center gap-2">
-          <Checkbox id="remember" />
-          <Label htmlFor="remember" className="text-white">
-            Remember me
-          </Label>
-        </div>
-        <Button type="submit">
+        <button
+          type="submit"
+          className="rounded-lg bg-[#4975dd] p-3 text-[#060606]"
+        >
           {loading ? (
             <>
-              <Spinner size="sm" /> Loading...
+              <Spinner size="sm" />
             </>
           ) : (
             "Sign In"
           )}
-        </Button>
+        </button>
+        <div className="flex items-center justify-between gap-2">
+          <p className="text-white">Don{`'`}t have an account?</p>
+          <Link to={"/signup"} className="text-blue-500">
+            Sign Up
+          </Link>
+        </div>
         <hr />
         <div
-          className="box-border flex cursor-pointer flex-col gap-2 bg-[#333] p-4 text-[#7e7e81]"
+          className="box-border flex cursor-pointer flex-col gap-2 bg-[#262626] p-4 text-[#7e7e81]"
           onClick={handleAdminLogin}
         >
           <p>Click me logging in as an admin</p>
@@ -127,6 +135,6 @@ export default function SignIn() {
           <p>Password : adminpassword</p>
         </div>
       </form>
-    </Card>
+    </div>
   );
 }
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,11 +1,4 @@
-import {
-  Button,
-  Checkbox,
-  Label,
-  TextInput,
-  Card,
-  Spinner,
-} from "flowbite-react";
+import { Spinner } from "flowbite-react";
 import { Link, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { useDispatch, useSelector } from "react-redux";
@@ -13,7 +6,11 @@ import { signUpFailure, signUpStart, signUpSuccess } from "../slice/userSlice";
 import { toast } from "react-toastify";
 
 export default function SignUp() {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   const dispatch = useDispatch();
   const { loading } = useSelector((state) => state.user);
   const navigate = useNavigate();
@@ -48,89 +45,99 @@ export default function SignUp() {
   };
 
   return (
-    <Card className="flex max-w-md items-center justify-center bg-[#1a1a1a]">
+    <div className="flex items-center justify-center p-4 sm:p-12">
       <form
-        className="flex max-w-md flex-col gap-4"
+        className="flex max-w-full flex-grow flex-col gap-4 rounded-xl border-[1px] p-6 outline-gray-500/50 sm:p-8 md:max-w-[60%] md:p-10 lg:max-w-[45%] lg:p-12"
         onSubmit={handleSubmit(onSubmit)}
       >
-        <div className="flex flex-col items-center gap-2">
-          <p className="text-2xl font-bold text-white">
-            Log in to your Account
-          </p>
-          <p className="text-lg font-bold text-[#7e7e81]">
-            Welcome Back! Please Enter Your Details
-          </p>
+        <div className="mt-2 flex flex-col items-center justify-center gap-2 text-xl font-semibold">
+          <span className="text-3xl font-bold">Log in to your Account</span>
+          Welcome Back! Please Enter Your Details
         </div>
-        <div>
-          <div className="mb-2 block">
-            <Label
-              htmlFor="useremail"
-              value="Your email"
-              className="text-white"
-            />
-          </div>
-          <TextInput
+        <div className="flex flex-col gap-2">
+          <label htmlFor="useremail" className="text-lg font-semibold">
+            Your Email
+          </label>
+          <input
             id="useremail"
+            name="useremail"
             type="email"
+            autoComplete="email"
             placeholder="name@flowbite.com"
             required
-            {...register("useremail", { required: true })}
+            className="rounded-lg bg-[#262626] p-2 font-thin placeholder:font-thin placeholder:text-[#b3b3b3] focus:outline-none"
+            {...register("useremail", { required: "User Email is required" })}
           />
+          {errors.useremail && (
+            <p className="font-semibold text-red-600">
+              <span className="font-bold">Oops </span>
+              {errors.useremail.message}
+            </p>
+          )}
         </div>
-        <div>
-          <div className="mb-2 block">
-            <Label
-              htmlFor="username"
-              value="User Name"
-              className="text-white"
-            />
-          </div>
-          <TextInput
+        <div className="flex flex-col gap-2">
+          <label htmlFor="username" className="text-lg font-semibold">
+            Your Name
+          </label>
+          <input
             id="username"
+            name="username"
+            autoComplete="username"
             type="text"
-            placeholder="username"
+            placeholder="Enter your User Name"
             required
-            {...register("username", { required: true })}
+            className="rounded-lg bg-[#262626] p-2 font-thin placeholder:font-thin placeholder:text-[#b3b3b3] focus:outline-none"
+            {...register("username", { required: "User name is required" })}
           />
+          {errors.username && (
+            <p className="font-semibold text-red-600">
+              <span className="font-bold">Oops </span>
+              {errors.username.message}
+            </p>
+          )}
         </div>
-        <div>
-          <div className="mb-2 block">
-            <Label
-              htmlFor="password1"
-              value="Your password"
-              className="text-white"
-            />
-          </div>
-          <TextInput
-            id="password1"
+
+        <div className="flex flex-col gap-2">
+          <label htmlFor="password" className="text-lg font-semibold">
+            Password
+          </label>
+          <input
+            id="password"
+            name="password"
             type="password"
+            autoComplete="new-password"
             required
-            {...register("password", { required: true })}
+            className="rounded-lg bg-[#262626] p-2 font-thin placeholder:font-thin placeholder:text-[#b3b3b3] focus:outline-none"
+            {...register("password", {
+              required: "Password is a necessary field!",
+            })}
           />
+          {errors.password && (
+            <p className="font-semibold text-red-600">
+              <span className="font-bold">Oops </span>
+              {errors.password.message}
+            </p>
+          )}
         </div>
-        <div className="flex items-center gap-2">
-          <Checkbox id="remember" />
-          <Label htmlFor="remember" className="text-white">
-            Remember me
-          </Label>
-        </div>
-        <Button type="submit">
+        <button
+          type="submit"
+          className="rounded-lg bg-[#4975dd] p-3 text-[#060606]"
+        >
           {loading ? (
             <>
-              <Spinner size="sm" /> Loading...
+              <Spinner size="sm" />
             </>
           ) : (
             "Sign Up"
           )}
-        </Button>
-        <hr />
-        <div className="flex items-center justify-around gap-2">
+        </button>
+        <div className="flex items-center justify-between gap-2">
           <p className="text-white">Already have an account?</p>
           <Link to={"/signin"} className="text-blue-500">
             Sign In
           </Link>
         </div>
       </form>
-    </Card>
+    </div>
   );
 }
