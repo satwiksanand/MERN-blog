e01c7cb2e4ba33de492ea14f9815e6c38682de75
satwik sanand <sanandsatwik421@gmail.com>
2024-11-17 12:12:04 +0000

    fixed some minor bugs

--- frontend/src/pages/Create.jsx
+++ frontend/src/pages/Create.jsx
@@ -122,27 +122,20 @@ function Create() {
           </div>
           <div className="hidden md:block">
             <div className="max-w-md rounded-lg border border-gray-200 bg-[#444] shadow dark:border-gray-700 dark:bg-gray-800">
-              <a href="#">
-                <img
-                  className="rounded-t-lg"
-                  src="https://plus.unsplash.com/premium_photo-1680680514571-e16201d23e9d?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bGFwdG9wJTIwb24lMjBkZXNrfGVufDB8fDB8fHww"
-                  alt=""
-                />
-              </a>
+              <img
+                className="rounded-t-lg"
+                src="https://plus.unsplash.com/premium_photo-1680680514571-e16201d23e9d?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bGFwdG9wJTIwb24lMjBkZXNrfGVufDB8fDB8fHww"
+                alt=""
+              />
               <div className="p-5">
-                <a href="#">
-                  <h5 className="text-#7e7e81 mb-2 text-2xl font-bold tracking-tight dark:text-white">
-                    Noteworthy technology acquisitions 2021
-                  </h5>
-                </a>
+                <h5 className="text-#7e7e81 mb-2 text-2xl font-bold tracking-tight dark:text-white">
+                  Noteworthy technology acquisitions 2021
+                </h5>
                 <p className="mb-3 font-normal text-white dark:text-gray-400">
                   Here are the biggest enterprise technology acquisitions of
                   2021 so far, in reverse chronological order.
                 </p>
-                <a
-                  href="#"
-                  className="text-md inline-flex items-center rounded-lg bg-blue-700 px-3 py-2 text-center font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
+                <div className="text-md inline-flex items-center rounded-lg bg-blue-700 px-3 py-2 text-center font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                   Read more
                   <svg
                     className="ms-2 h-3.5 w-3.5 rtl:rotate-180"
@@ -159,7 +152,7 @@ function Create() {
                       d="M1 5h12m0 0L9 1m4 4L9 9"
                     />
                   </svg>
-                </a>
+                </div>
               </div>
             </div>
           </div>
